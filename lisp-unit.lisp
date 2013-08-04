f4510b2b0672016f31b08c95ff203779b955860e
Jacek <ruby.object@googlemail.com>
2013-08-05 00:17:01 +0100

    thinking about adding instruction reference

--- lisp-unit.lisp
+++ lisp-unit.lisp
@@ -17,7 +17,7 @@
   (assert-equal "10101010" (int-to-bin 170)))
 
 (define-test mod-rm
-  "testing correct values of modrm and sib byte fiels"
+    "testing correct values of modrm and sib byte fiels"
   (assert-equal 1 (mod-part "6b"))
   (assert-equal 3 (rm-part "6b"))
   (assert-equal 5 (reg-part "6b"))
@@ -28,4 +28,18 @@
   (assert-equal 2 (ss-part "a6"))
   (assert-equal 4 (index-part "a6"))
   (assert-equal 6 (base-part "a6"))
+  )
+
+
+(define-test instructions
+    "instruction reference"
+  
+  37 AAA ASCII adjust AL after addition
+
+  D5 0A AAD ASCII adjust AX before division
+  D5 ib No-mnemonic Adjust AX before division to number base imm8
+
+
+  D4 0A AAM ASCII adjust AX after multiply
+  D4 ib No-mnemonic Adjust AX after multiply to number base imm8
   )
