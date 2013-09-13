b0ce7a73013be512daa8f19bef5ee2de6353ab05
Jacek Podkanski <ruby.object@googlemail.com>
2013-09-13 17:31:41 +0100

    very verbose warnings

--- constants.lisp
+++ constants.lisp
@@ -2,10 +2,15 @@
 
 (defmacro define-constant (name value &optional doc)
   (if (boundp name)
-      (format t
-              "~&already defined ~A~%old value ~s~%attempted value ~s~%"
-              name (symbol-value name) value))
-  `(defconstant ,name (if (boundp ',name) (symbol-value ',name) ,value)
-     ,@(when doc (list doc))))
+      (progn
+        (if (not (equalp (symbol-value name) value)) 
+            (format 
+             t "~&warning: constant ~A defined as~&~s attempted value~&~s~%"
+             name (symbol-value name) value)
+            (format
+             t "~&redefining constant ~S with the same value ~S~%"
+             name value))
+        `(defconstant ,name (if (boundp ',name) (symbol-value ',name) ,value)
+           ,@(when doc (list doc))))))
 
 (define-constant +dll-folder+ "~/.wine/drive_c/windows/system32")
