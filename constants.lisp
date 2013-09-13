5e2bb8a5c47689cbaedab16dd501818a4c3ba70c
Jacek Podkanski <ruby.object@googlemail.com>
2013-09-13 20:40:41 +0100

    fixed an error with the constant identification macro

--- constants.lisp
+++ constants.lisp
@@ -2,12 +2,11 @@
 
 (defmacro define-constant (name value &optional doc)
   (if (boundp name)
-      (progn
-        (unless (equalp (symbol-value name) value)
-            (format
-             t "~&warning: constant ~A defined as~&~s attempted value~&~s~%"
-             name (symbol-value name) value))
-        `(defconstant ,name (if (boundp ',name) (symbol-value ',name) ,value)
-           ,@(when doc (list doc))))))
+      (unless (equalp (symbol-value name) value)
+        (format t
+         "~&warning: constant ~A defined as:~&~s attempted redefining as:~&~s~%"
+         name (symbol-value name) value)))
+  `(defconstant ,name (if (boundp ',name) (symbol-value ',name) ,value)
+     ,@(when doc (list doc))))
 
 (define-constant +dll-folder+ "~/.wine/drive_c/windows/system32")
