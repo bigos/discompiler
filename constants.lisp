42a3dc6b7403595ab55edac6256d0de55f2e58e8
Jacek Podkanski <ruby.object@googlemail.com>
2013-09-13 16:53:45 +0100

    added macro for portable defining of constants

--- constants.lisp
+++ constants.lisp
@@ -1,3 +1,11 @@
 (in-package :discompiler)
 
-(defconstant +dll-folder+ "~/.wine/drive_c/windows/system32")
+(defmacro define-constant (name value &optional doc)
+  (if (boundp name)
+      (format t
+              "~&already defined ~A~%old value ~s~%attempted value ~s~%"
+              name (symbol-value name) value))
+  `(defconstant ,name (if (boundp ',name) (symbol-value ',name) ,value)
+     ,@(when doc (list doc))))
+
+(define-constant +dll-folder+ "~/.wine/drive_c/windows/system32")
