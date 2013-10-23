bca0b41a1440f6238660a623e923ceba49f575ff
Jacek Podkanski <ruby.object@googlemail.com>
2013-10-23 15:44:21 +0100

    started working on loader

---
+++ loader.lisp
@@ -0,0 +1,20 @@
+(in-package :discompiler)
+
+
+#|
+Read first page of the file which includes DOS header, PE header, section
+headers etc.
+
+Fetch Image Base address from PE header and determine if that address is
+available else allocate another area.  (Case of relocation)
+
+Map the sections into the allocated area
+
+Read information from import table and load the DLLs
+
+Resolve the function addresses and create Import Address Table (IAT).
+
+Create initial heap and stack using values from PE header.
+
+Create main thread and start the process.
+|#
