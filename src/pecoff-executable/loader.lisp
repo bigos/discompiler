67e182d124840c02e907a224d7ba02f71fb34f0f
Jacek Podkanski <ruby.object@googlemail.com>
2014-01-29 21:34:32 +0000

    tried some refactoring

--- src/pecoff-executable/loader.lisp
+++ src/pecoff-executable/loader.lisp
@@ -19,15 +19,21 @@
         (c-structure-values bytes elements offset)
       (values data structure-size))))
 
+(defun rva-addr-in-struct (struct-name struct bytes &optional (offset 0))
+  (rva-addr
+   (+ (struct-value struct-name struct)
+      offset)
+   bytes))
+
 (defun library-name (mem bytes directory-table)
   (declare (optimize (speed 0) (space 1) (compilation-speed 0) (debug 3)))
   (concatenate 'string ""
                (loop for offset from 0 by 1
-                  for c = (get-allocated mem (rva-addr
-                                              (+ offset
-                                                 (struct-value "NameRVA"
-                                                               directory-table))
-                                              bytes ))
+                  for c = (get-allocated mem
+                                         (rva-addr-in-struct "NameRVA"
+                                                             directory-table
+                                                             bytes
+                                                             offset))
                   until (zerop c)
                   collecting  (code-char c))))
 
@@ -59,74 +65,49 @@
                                    while (not (zerop c))
                                    collecting (code-char c)))))))
 
+(defun loader-data (bytes mem import-table-size)
+  (progn
+    (format t "import table RVA directory bytes~%~S~%"
+            (get-rva-table-bytes bytes
+                                 mem
+                                 "Import Table RVA"
+                                 "Import Table Size"))
+    (loop
+       for offset from 0 by import-table-size
+       for idt = (import-directory-table
+                  (get-rva-table-bytes bytes
+                                       mem
+                                       "Import Table RVA"
+                                       "Import Table Size") offset)
+       until (zerop  (struct-value "ImportLookupTableRVA" idt))
+       do
+         (format t "import directory table ~S~%" idt)
+         (format t "import address table ~X~%" (struct-value "ImportAddressTableRVA" idt))
+         (format t "~S ~%" (library-name mem bytes idt))
+         (format t "import lookup table ~X~%"
+                 (rva-addr-in-struct  "ImportLookupTableRVA" idt bytes ))
+         (format t "~%")
+         (imported-function-names mem bytes idt))))
+
 (defun loader (bytes)
   (let* ((mem (make-instance 'memory :start #x110000 :end #xFFFF0001))
          (import-table-size (multiple-value-bind (d s)
                                 (import-directory-table bytes 0)
-                              (declare (ignore d))
-                              s)))
-
-    (format t "free memory ~S image base ~S ~%" (find-free mem) (image-base bytes))
-    ;; Read first page of the file which includes DOS header, PE header, section
-    ;; headers etc.
-
-    ;; Fetch Image Base address from PE header and determine if that address is
-    ;; available else allocate another area.  (Case of relocation)
-
-    ;; Map the sections into the allocated area
+                              (declare (ignore d)) s)))
     (allocate-and-load-sections bytes mem)
     ;; TODO memory blocks are still reversed in memory object
-
     (if (zerop (optional-header-value bytes "Import Table RVA"))
         (princ " zero import RVA detected")
-        (progn
-          (format t "import table RVA directory bytes~%~S~%"
-                  (get-rva-table-bytes bytes
-                                       mem
-                                       "Import Table RVA"
-                                       "Import Table Size"))
-
-
-          (loop
-             for offset from 0 by import-table-size
-             for idt = (import-directory-table
-                        (get-rva-table-bytes bytes
-                                             mem
-                                             "Import Table RVA"
-                                             "Import Table Size") offset)
-             until (zerop  (struct-value "ImportLookupTableRVA" idt))
-             do
-               (format t "import directory table ~S~%" idt)
-             ;; need to check why malformed string is being returned
-               (format t "import address table ~X~%" (struct-value "ImportAddressTableRVA" idt))
-               (format t "~S ~%" (library-name mem bytes idt))
-               (format t "import lookup table ~X~%" (rva-addr
-                                                     (struct-value "ImportLookupTableRVA" idt)
-                                                     bytes))
-               (format t "~%")
-               (imported-function-names mem bytes idt)
-               )))
-    (princ " after loop ")
-    ;; don't show it for now, less problems with large files
-    ;; (format t "resource table RVA~%~S~%"
-    ;;         (get-rva-table-bytes bytes
-    ;;                              mem
-    ;;                              "Resource Table RVA"
-    ;;                              "Resource Table Size"))
+        (loader-data bytes mem import-table-size))
     (if (zerop (optional-header-value bytes "IAT RVA"))
         (princ " zero IAT rva detected ")
         (format t "IAT RVA~%~S~%"
                 (get-rva-table-bytes bytes mem "IAT RVA" "IAT Size" )))
 
-
     ;; Read information from import table and load the DLLs
-
     ;; Resolve the function addresses and create Import Address Table (IAT).
-
     ;; Create initial heap and stack using values from PE header.
-
     ;; Create main thread and start the process.
-
     mem))
 
 
@@ -157,27 +138,26 @@
 
 (defun export-address-table (bytes memory edt)
   (loop for ate from 0 to (1- (struct-value "AddressTableEntries" edt))
-     for offset = (rva-addr  (+ (struct-value "ExportAddressTableRVA" edt)
-                                (* ate  +long+))
-                             bytes)
+     for offset = (rva-addr-in-struct "ExportAddressTableRVA" edt bytes (* ate +long+))
+
      for a = (bytes-to-type-int (get-allocated-bytes memory offset 4))
      collect
        (rva-addr a bytes)))
 
 (defun name-pointer-table (bytes memory edt)
   (loop for npe from 0 to (1- (struct-value "NumberOfNamePointers" edt))
-     for y = (rva-addr (+ (struct-value "NamePointerRVA" edt) (* npe +long+))
-                       bytes)
+     for y = (rva-addr-in-struct "NamePointerRVA" edt bytes (* npe +long+))
+
      for a = (bytes-to-type-int (get-allocated-bytes memory y 4))
      collect
        (get-allocated-string memory (rva-addr a bytes))))
 
 (defun export-ordinal-table (bytes memory edt)
+  (declare (optimize (speed 0) (space 1) (compilation-speed 0) (debug 3)))
   (loop for npe from 0 to (1- (struct-value "NumberOfNamePointers" edt))
-     for y = (rva-addr (+ (struct-value "OrdinalTableRVA" edt) (* npe +short+))
-                       bytes)
-     for a =  (bytes-to-type-int
-               (get-allocated-bytes memory y +short+))
+     for y = (rva-addr-in-struct "OrdinalTableRVA" edt bytes (* npe +short+))
+     for a = (bytes-to-type-int
+              (get-allocated-bytes memory y +short+))
      collect (cons a (+ a (struct-value "OrdinalBase" edt)))))
 
 (defun address-to-code (export-list table-pos)
@@ -223,28 +203,24 @@
                                         "Export Table Size") 0))
              (address-table (export-address-table bytes memory edt))
              (name-table (name-pointer-table bytes memory edt))
-             (ordinal-table (export-ordinal-table bytes memory edt))
-             (results))
+             (ordinal-table (export-ordinal-table bytes memory edt)))
 
-        (format t "memory blocks ~S~%" (blocks memory))
-        (format t "~S  ~%~S ~S~%entries ~S names ~S address table hex addr  ~S~%"
-                edt
-                (get-allocated-string  memory
-                                       (rva-addr
-                                        (struct-value
-                                         "NameRVA"
-                                         edt)
-                                        bytes))
-                (struct-value "OrdinalBase" edt)
-                (struct-value "AddressTableEntries" edt)
-                (struct-value "NumberOfNamePointers" edt)
-                (hex-rva-addr  (struct-value "ExportAddressTableRVA" edt) bytes))
-        (format t "~&address table entries~%")
-        (format t "item addr, val, rva, forwarding, result~%~s~%"
-                address-table)
-        (format t "~&name pointer entries~%~S~%"
-                name-table)
-        (format t "~&Export ordinal table~%~S~%"
-                ordinal-table)
-        (setf results (list address-table name-table ordinal-table))
+        ;; (format t "memory blocks ~S~%" (blocks memory))
+        ;; (format t "~S  ~%~S ~S~%entries ~S names ~S address table hex addr  ~S~%"
+        ;;         edt
+        ;;         (get-allocated-string  memory
+        ;;                                (rva-addr
+        ;;                                 (struct-value
+        ;;                                  "NameRVA"
+        ;;                                  edt)
+        ;;                                 bytes))
+        ;;         (struct-value "OrdinalBase" edt)
+        ;;         (struct-value "AddressTableEntries" edt)
+        ;;         (struct-value "NumberOfNamePointers" edt)
+        ;;         (hex-rva-addr  (struct-value "ExportAddressTableRVA" edt) bytes))
+        ;; (format t "~&address table entries~%")
+        ;; (format t "item addr, val, rva, forwarding, result~%~s~%" address-table)
+        ;; (format t "~&name pointer entries~%~S~%" name-table)
+        ;; (format t "~&Export ordinal table~%~S~%" ordinal-table)
+        (list address-table name-table ordinal-table)
         )))
