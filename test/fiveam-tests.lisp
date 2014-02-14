217d935c7e2203f4bb872adabd0994836ae7c667
Jacek Podkanski <ruby.object@googlemail.com>
2014-02-14 17:19:55 +0000

    fiveam tests work

--- test/fiveam-tests.lisp
+++ test/fiveam-tests.lisp
@@ -8,14 +8,14 @@
 
 (test test-addition
       "test simple addition"
-      (is (= 3) (+ 1 2))
-      (is (= 1) (+ 0 1))
-      (is (= 17) (1- (* 3 6)))
-      (is (= 6) (/ 6 1))
-      (is (= 6) (+ 1 2 3)))
+      (is (= 3 (+ 1 2)))
+      (is (= 1 (+ 0 1)))
+      (is (= 17 (1- (* 3 6))))
+      (is (= 6 (/ 6 1)))
+      (is (= 6 (+ 1 2 3))))
 
 (test add-2
   "Test the ADD-2 function" ;; a short description
   ;; the checks
-  (is (= 2 (+ 2 0)))
+  (is (= 2 (+ 2 (- 5 5))))
   (is (= 0 (+ 2 -2))))
