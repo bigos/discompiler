7fa8ad6d2c8ce6fc1601800802f929b95fe4ce60
Jacek Podkanski <ruby.object@googlemail.com>
2014-03-31 01:12:14 +0100

    filled in test data

--- test/pe-coff-tests.lisp
+++ test/pe-coff-tests.lisp
@@ -271,13 +271,41 @@
     (is (equalp '((#x110000 . #x3FFFFF)
                   (#x405000 . #xffff0000)) (find-free new-mem)))
 
-    (is (equalp
-         (list (make-module
-                :BASEDLLNAME "crackme12"
-                :DLLBASE 4194304
-                :FULLDLLNAME "~/discompiler/SampleExecutables/PE/crackme12.exe"
-                :ORIGINALBASE 4194304
-                :SIZEOFIMAGE 20480))  (modules new-mem)))
+    (is (equalp (nth 0 (modules new-mem))
+                (make-module
+                 :BASEDLLNAME "crackme12"
+                 :DLLBASE #x400000
+                 :FULLDLLNAME "~/discompiler/SampleExecutables/PE/crackme12.exe"
+                 :ORIGINALBASE #x 400000
+                 :SIZEOFIMAGE #x5000)))
+    (is (equalp (nth 1 (modules new-mem))
+                (make-module
+                 :BASEDLLNAME "USER32"
+                 :DLLBASE #x77d40000
+                 :FULLDLLNAME "~/discompiler/SampleExecutables/PE/DLLs/user32.dll"
+                 :ORIGINALBASE #x77D40000
+                 :SIZEOFIMAGE #x90000)))
+    (is (equalp (nth 2 (modules new-mem))
+                (make-module
+                 :BASEDLLNAME "GDI32"
+                 :DLLBASE #x77f10000
+                 :FULLDLLNAME "~/discompiler/SampleExecutables/PE/DLLs/gdi32.dll"
+                 :ORIGINALBASE #x77F10000
+                 :SIZEOFIMAGE #x46000)))
+    (is (equalp (nth 3(modules new-mem))
+                (make-module
+                 :BASEDLLNAME "kernel32"
+                 :DLLBASE #x7c800000
+                 :FULLDLLNAME "~/discompiler/SampleExecutables/PE/DLLs/kernel32.dll"
+                 :ORIGINALBASE #x7C800000
+                 :SIZEOFIMAGE #xF4000)))
+    (is (equalp (nth 4 (modules new-mem))
+                (make-module
+                 :BASEDLLNAME "ntdll"
+                 :DLLBASE #x7c900000
+                 :FULLDLLNAME "~/discompiler/SampleExecutables/PE/DLLs/ntdll.dll"
+                 :ORIGINALBASE #x7C900000
+                 :SIZEOFIMAGE  #xB0000)))
     ;; it's not loading libraries yet,
     ;; so there's only information about the executable
     ))
