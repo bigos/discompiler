2926c7d463ead82c1836c52f6d6e06957b3b48a7
Jacek Podkanski <ruby.object@googlemail.com>
2014-04-01 03:11:30 +0100

    more cleanup

--- src/pecoff-executable/imports.lisp
+++ src/pecoff-executable/imports.lisp
@@ -60,17 +60,18 @@
   (let ((import-table-size (multiple-value-bind (d s)
                                (import-directory-table bytes 0)
                              (declare (ignore d)) s)))
-    (loop
-       for offset from 0 by import-table-size
-       for rva-bytes = (get-rva-table-bytes bytes
-                                            mem
-                                            "Import Table RVA"
-                                            "Import Table Size")
-       for idt = (import-directory-table rva-bytes offset)
-       for imp-dir-rva = (struct-value "ImportLookupTableRVA" idt)
-       until (zerop imp-dir-rva)
-       collect
-         (list
-          (library-name mem bytes idt)
-          (imported-function-names mem bytes imp-dir-rva))
-         )))
+    (unless (zerop (optional-header-value bytes "Import Table RVA"))
+      (loop
+         for offset from 0 by import-table-size
+         for rva-bytes = (get-rva-table-bytes bytes
+                                              mem
+                                              "Import Table RVA"
+                                              "Import Table Size")
+         for idt = (import-directory-table rva-bytes offset)
+         for imp-dir-rva = (struct-value "ImportLookupTableRVA" idt)
+         until (zerop imp-dir-rva)
+         collect
+           (list
+            (library-name mem bytes idt)
+            (imported-function-names mem bytes imp-dir-rva))
+           ))))
