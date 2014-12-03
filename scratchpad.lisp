a64c27b07c8eb558f2f68f80cd2c07507bcdb0dd
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-12-03 15:39:34 +0000

    started scratchpad work

--- scratchpad.lisp
+++ scratchpad.lisp
@@ -1,7 +1,11 @@
 ;; This file is for temporary Lisp code, for situations where REPL is not convenient enough
 ;; Once the code matures, it should go to source folder.
 
-(format t "~%~%*+*+*+*+*+*+*+*+*~%~%")
 
-(defun boo ()
-  (format t "~%boobooboobooboobooboo~%~%"))
+
+(defun investigate ()
+  (let* ((file "~/discompiler/SampleExecutables/PE/myfavlibrary.exe")
+         (bytes (file-to-bytes file))
+         (mem (loader bytes))
+         (imports (imported-functions bytes mem)))
+    (format t "~&~A~%" (nth 17 imports))))
