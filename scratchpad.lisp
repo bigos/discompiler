59edd83fdfe8040036ce4d0dce5d4be9ff784c49
J. Podkanski <ruby.object@googlemail.com>
2014-12-10 09:00:54 +0000

    wip experiments

--- scratchpad.lisp
+++ scratchpad.lisp
@@ -1,11 +1,15 @@
 ;; This file is for temporary Lisp code, for situations where REPL is not convenient enough
 ;; Once the code matures, it should go to source folder.
 
-
-
 (defun investigate ()
+  (declare (debug 3))
   (let* ((file "~/discompiler/SampleExecutables/PE/myfavlibrary.exe")
          (bytes (file-to-bytes file))
          (mem (loader bytes))
          (imports (imported-functions bytes mem)))
+    (cerror "aaa" "bbb")
     (format t "~&~A~%" (nth 17 imports))))
+
+;;;
+aaaa
+bbb
