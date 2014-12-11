354e7c73d03598ba21f9887da5cbc9f45b1421ff
Jacek Podkanski <jacek.podkanski@chess.uk.com>
2014-12-11 12:17:41 +0000

    got to interesting point

--- scratchpad.lisp
+++ scratchpad.lisp
@@ -6,8 +6,28 @@
   (let* ((file "~/discompiler/SampleExecutables/PE/myfavlibrary.exe")
          (bytes (file-to-bytes file))
          (mem (loader bytes))
-         (imports (imported-functions bytes mem)))
-    (cerror "aaa" "bbb")
+         ;; (imports (imported-functions bytes mem))
+         )
+    (let ((import-table-size (multiple-value-bind (d s)
+                                 (import-directory-table bytes 0)
+                               (declare (ignore d)) s)))
+      (unless (zerop (optional-header-value bytes "Import Table RVA"))
+        (loop
+           for offset from 0 by import-table-size
+           for rva-bytes = (get-rva-table-bytes bytes
+                                                mem
+                                                "Import Table RVA"
+                                                "Import Table Size")
+           for idt = (import-directory-table rva-bytes offset)
+           for imp-dir-rva = (struct-value "ImportLookupTableRVA" idt)
+           until (zerop imp-dir-rva)
+           do (when (eq offset 340)
+                (cerror "as normal" "investigate me"))
+           collect
+             (list
+              (library-name mem bytes idt)
+              (imported-function-names mem bytes imp-dir-rva))
+             )))
     (format t "~&~A~%" (nth 17 imports))))
 
 ;;;
